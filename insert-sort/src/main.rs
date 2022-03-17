a6c16b1a3d789c209b575b85a738d32e03c8c070
Frog <parachutefrog@gmail.com>
2022-03-17 23:17:44 +0900

    clean: 挿入ソートを関数化

--- insert-sort/src/main.rs
+++ insert-sort/src/main.rs
@@ -12,31 +12,39 @@ fn main() {
     const MAX:i32 = 1001; 
     let randvec = create_randvec(VEC_SIZE, MIN, MAX);
     println!("\n ソート前の配列は \n {:?} \n です", &randvec);
-    let mut sorted = Vec::new();
-    sorted.push(&randvec[0]);
-    println!("sort start! {:?}",sorted);
-    for j in 1..randvec.len(){
-        let mut is_inserted= false;
-        for i in 0..sorted.len(){
-            if &randvec[j] <= sorted[i]{
-            sorted.insert(i, &randvec[j]);
-            println!("{} is inserted in [{}] {:?}",&randvec[j],i,sorted);
-            is_inserted = true;
-            break;
-            }
-        }
-        if !is_inserted {
-            sorted.push(&randvec[j]);
-            println!("{} is inserted in end {:?}",&randvec[j],sorted);
-        }
-    }
-    println!("\n ソート後の配列は \n {:?} \n です", sorted);
+    
+    let sortedvec = insert_sort(&randvec);
+    println!("\n ソート後の配列は \n {:?} \n です", &sortedvec);
 }
 
+
+// ランダム配列を生成する関数
 fn create_randvec(size:usize, min:i32, max:i32) -> Vec<i32>{
     let mut randvec = Vec::new();
     for _i in 0..size{
         randvec.push(rand::thread_rng().gen_range(min, max));
     }
     randvec
+}
+// 配列(ベクタ)を挿入ソートして返す関数
+fn insert_sort(randvec:&Vec<i32>) -> Vec<i32>{
+    let mut sorted = Vec::new();
+    sorted.push(randvec[0]);
+    println!("sort start! {:?}",sorted);
+    for j in 1..randvec.len(){
+        let mut is_inserted= false;
+        for i in 0..sorted.len(){
+            if randvec[j] <= sorted[i]{
+            sorted.insert(i, randvec[j]);
+            println!("{} is inserted in [{}] {:?}",&randvec[j],i,sorted);
+            is_inserted = true;
+            break;
+            }
+        }
+        if !is_inserted {
+            sorted.push(randvec[j]);
+            println!("{} is inserted in end {:?}",&randvec[j],sorted);
+        }
+    }
+    sorted
 }
